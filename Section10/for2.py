f9a9489fae35f92a3088e4b4bcb889b162caa2a7
unknown <blk97tt@hotmail.com>
2023-03-16 08:43:12 -0500

    section 10

---
+++ Section10/for2.py
@@ -0,0 +1,6 @@
+my_string = "Cisco"
+
+for letter in my_string:
+    print(letter)
+    print(letter * 2)
+    print(letter * 3)
---
+++ Section10/for3.py
@@ -0,0 +1,6 @@
+vendors = ["Cisco", "HP", "Nortel", "Avaya", "Juniper"]
+
+for element in vendors:
+    print(element)
+else:
+    print("The end of the list has been reached")
